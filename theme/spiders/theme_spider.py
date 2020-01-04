4f9e0bd03967cd177e4627aec465735276e54fb7
Yuanjie <380774061@qq.com>
2020-01-04 10:03:51 +0800

    Get theme data fix#5

--- theme/spiders/theme_spider.py
+++ theme/spiders/theme_spider.py
@@ -12,8 +12,16 @@ class ThemeSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        self.log('------Saved theme---------')
-        # self.log('%s' % response.body)
-        for theme in response.css(".plugin"):
-            self.log(theme.css(".plugin-name::text").get())
-            self.log(theme.css(".plugin-name::attr(href)").get())
+        for theme in response.css(".plugin.on"):
+            theme_name = theme.css(".plugin-name::text").get()
+            theme_url = theme.css(".plugin-name::attr(href)").get()
+            yield scrapy.Request(url=theme_url, callback=self.parse_github, cb_kwargs={
+                'name': theme_name,
+                'url': theme_url})
+
+    def parse_github(self, response, name, url):
+        data = response.css(".social-count::attr(aria-label)").getall()
+        watch = data[0].split(' ')[0]
+        star = data[1].split(' ')[0]
+        folk = data[2].split(' ')[0]
+        self.log("The theme's name is %s , watch is %s, star is %s , fold is %s, the url is %s." % (name, watch, star, folk, url))
